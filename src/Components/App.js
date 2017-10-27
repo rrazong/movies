37b4d8e8775cb9787ce29299049366613cf539a4
Richelle Razon <rrazony@yahoo.com>
2017-10-27 10:02:05 -0700

    Initial commit

---
+++ src/Components/App.js
@@ -0,0 +1,16 @@
+'use strict';
+
+import React, { Component } from 'react';
+
+//css
+require('style-loader!./App.css');
+
+class App extends Component {
+  render() {
+    return (
+      <h1>Movie Reviews</h1>
+    );
+  }
+}
+
+export default App;
